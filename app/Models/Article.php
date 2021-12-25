6794e8ad8fbe2f1cb26d615f235ca6ca8bec7bbd
aytarozgur <zrhaytar@gmail.com>
2021-12-25 23:50:56 +0300

    relation with article and category

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Jenssegers\Mongodb\Eloquent\Model;
 
-class Article extends Eloquent
+class Article extends Model
 {
-    use HasFactory;
+    function getCategory(){
+      return $this->hasOne('App\Models\Category','id','category_id');
+    }
 }
